794f124e4ff260e16f42799dca2944cc2d69b2e6
derek williams <derek61@gmail.com>
2025-03-24 14:03:40 -0600

    update package.json to latest versions, and move eslint configuration to eslint.config.mjs

--- starter/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: "next/core-web-vitals",
-  plugins: ["@stylexjs"],
-  rules: {
-    // The Eslint rule still needs work, but you can
-    // enable it to test things out.
-    "@stylexjs/valid-styles": "error",
-  },
-};
